d351bb640114f0c0c55d21ac5355ca4bf5e5e445
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-10-14 19:32:36 -0300

    feat: connect to mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
+mongoose
+  .connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const port = 3001;
 
-const port = 3001;
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch(() => console.log('Error to connect to mongodb'));
