de9b8eb09cfa06ae8c2838c14244f7397f32deb7
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-10-14 20:38:51 -0300

    feat: add create category route

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ mongoose
     const app = express();
     const port = 3001;
 
+    app.use(express.json());
     app.use(router);
 
     app.listen(port, () => {
--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 
+import { createCategory } from './app/useCases/categories/createCategory';
 import { listCategories } from './app/useCases/categories/listCategories';
 
 export const router = Router();
 
 router.get('/categories', listCategories);
+
+router.post('/categories', createCategory);
