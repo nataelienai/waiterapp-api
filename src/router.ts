35c6bc04b9848b3e52b407d3709dc7055a26dcd7
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-10-14 20:45:04 -0300

    feat: add list products route

--- src/router.ts
+++ src/router.ts
@@ -2,9 +2,12 @@ import { Router } from 'express';
 
 import { createCategory } from './app/useCases/categories/createCategory';
 import { listCategories } from './app/useCases/categories/listCategories';
+import { listProducts } from './app/useCases/products/listProducts';
 
 export const router = Router();
 
 router.get('/categories', listCategories);
 
 router.post('/categories', createCategory);
+
+router.get('/products', listProducts);
