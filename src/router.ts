071801878f10b0f268e6cf26fa82058781b819bb
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-10-14 22:19:21 -0300

    feat: add create product route

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,30 @@
+import path from 'node:path';
+
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategory } from './app/useCases/categories/createCategory';
 import { listCategories } from './app/useCases/categories/listCategories';
+import { createProduct } from './app/useCases/products/createProduct';
 import { listProducts } from './app/useCases/products/listProducts';
 
 export const router = Router();
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, callback) {
+      callback(null, path.resolve(__dirname, '..', 'uploads'));
+    },
+    filename(req, file, callback) {
+      callback(null, `${Date.now()}-${file.originalname}`);
+    },
+  }),
+});
 
 router.get('/categories', listCategories);
 
 router.post('/categories', createCategory);
 
 router.get('/products', listProducts);
+
+router.post('/products', upload.single('image'), createProduct);
