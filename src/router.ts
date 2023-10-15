eab0c71b6bd95745182bb633c3b1c12cd6323fe0
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-10-15 11:06:07 -0300

    feat: add route to create order

--- src/router.ts
+++ src/router.ts
@@ -6,6 +6,7 @@ import multer from 'multer';
 import { createCategory } from './app/useCases/categories/createCategory';
 import { listCategories } from './app/useCases/categories/listCategories';
 import { listProductsByCategory } from './app/useCases/categories/listProductsByCategory';
+import { createOrder } from './app/useCases/orders/createOrder';
 import { listOrders } from './app/useCases/orders/listOrders';
 import { createProduct } from './app/useCases/products/createProduct';
 import { listProducts } from './app/useCases/products/listProducts';
@@ -34,3 +35,5 @@ router.post('/products', upload.single('image'), createProduct);
 router.get('/categories/:categoryId/products', listProductsByCategory);
 
 router.get('/orders', listOrders);
+
+router.post('/orders', createOrder);
