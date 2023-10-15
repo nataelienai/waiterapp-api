72906708ec2ea193685ad452d09f255facc1ba03
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-10-15 11:13:11 -0300

    feat: add sorting and product population to list orders

--- src/app/useCases/orders/listOrders.ts
+++ src/app/useCases/orders/listOrders.ts
@@ -4,7 +4,9 @@ import { Order } from '../../models/Order';
 
 export async function listOrders(req: Request, res: Response) {
   try {
-    const orders = await Order.find();
+    const orders = await Order.find()
+      .sort({ createdAt: 1 })
+      .populate('products.product');
 
     res.json(orders);
   } catch (error) {
