6a1b7919a53fa043b53677ecd7222a553baca6be
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-10-15 10:43:08 -0300

    feat: add route to serve uploaded images

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import path from 'node:path';
+
 import express from 'express';
 import mongoose from 'mongoose';
 
@@ -9,6 +11,10 @@ mongoose
     const app = express();
     const port = 3001;
 
+    app.use(
+      '/uploads',
+      express.static(path.resolve(__dirname, '..', 'uploads')),
+    );
     app.use(express.json());
     app.use(router);
 
