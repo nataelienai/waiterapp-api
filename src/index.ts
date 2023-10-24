6f08530d6704608d6ac4441bade1e61ee1d66c34
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-10-23 22:16:53 -0300

    feat: emit event for each new order via websocket

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,26 @@
+import http from 'node:http';
+
 import express from 'express';
 import mongoose from 'mongoose';
+import { Server } from 'socket.io';
 
 import { cors } from './app/middlewares/cors';
 import { router } from './router';
 
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
+
 mongoose
   .connect('mongodb://localhost:27017')
   .then(() => {
-    const app = express();
     const port = 3001;
 
     app.use(cors);
     app.use(express.json());
     app.use(router);
 
-    app.listen(port, () => {
+    server.listen(port, () => {
       console.log(`Server is running on http://localhost:${port}`);
     });
   })
